09d0e79eaa7960c5e41627e284f6967915ca675f
kwishna <kwishna@gmail.com>
2024-06-30 22:44:52 +0530

    first commit

---
+++ src/test/java/ai/spring/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.spring.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
